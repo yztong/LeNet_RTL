a8501b25accd8b34310fec7a6ddef9048f95293c
YzTong <yztong1994@qq.com>
2018-01-25 21:30:52 +0800

    Release 1.1

--- src/memory/f1_ram.v
+++ src/memory/f1_ram.v
@@ -1,7 +1,7 @@
 //==================================================================================================
 //  Filename      : f1_rom.v
 //  Created On    : 2017-12-29 19:14:58
-//  Last Modified : 2018-01-23 20:25:53
+//  Last Modified : 2018-01-25 18:14:57
 //  Revision      : 
 //  Author        : YzTong
 //  Company       : UESTC
@@ -21,13 +21,14 @@ module f1_ram(/*autoport*/
 			f1_wr_en,
 			f1_waddr,
 			f1_wdata,
+			ena,
 			f1_raddr);
 input wire			rclk;
 input wire			wclk;
 input wire [3:0]    f1_wr_en;
 input wire [31:0]   f1_waddr;
 input wire [31:0]   f1_wdata;
-
+input wire			ena;
 input wire [9:0]   f1_raddr;
 output wire [7:0]  f1_rdata; 
 
@@ -36,6 +37,7 @@ output wire [7:0]  f1_rdata;
 //2 cycles delay
 blk_mem_gen_f1_ram f1_ram (
   .clka(wclk),    // input wire clka
+  .ena(ena),  
   .wea(f1_wr_en),      // input wire [3 : 0] wea
   .addra(f1_waddr),  // input wire [31 : 0] addra
   .dina(f1_wdata),    // input wire [31 : 0] dina
