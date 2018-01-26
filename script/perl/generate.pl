1afe476173efd5239cbf9f9818eb6fdface83f1a
YzTong <yztong1994@qq.com>
2018-01-26 15:56:08 +0800

    change w1_rom w3_rom

--- script/perl/generate.pl
+++ script/perl/generate.pl
@@ -3,7 +3,14 @@ open my $gen_fh,'>','gen.txt'
 	or die "could not open gen.txt:$!";
 
 	for (my $j = 1; $j < 17; $j++) {
-			my $i = ($j-1)*16;
-			my $b = $i + 15;
-			print $gen_fh "	assign f4_$j\_rdata = f4_rdata[$b:$i];\n";
+
+		for (my $var = 1; $var <7; $var++){
+			
+			my $i = ($j-1)*48 + ($var-1)*8;
+			my $b = $i + 7;
+			print $gen_fh "	assign w3_$j\_$var\_rdata = w3_rdata[$b:$i];\n";
+
+
+		}
+			
 }
--- src/memory/w1_rom.v
+++ src/memory/w1_rom.v
@@ -1,7 +1,7 @@
 //==================================================================================================
 //  Filename      : weight_rom.v
 //  Created On    : 2017-12-29 19:12:21
-//  Last Modified : 2018-01-06 16:36:55
+//  Last Modified : 2018-01-26 15:47:36
 //  Revision      : 
 //  Author        : YzTong
 //  Company       : UESTC
@@ -32,36 +32,17 @@ module w1_rom(/*autoport*/
 	input wire  [4:0] w1_raddr;
 
 	input wire clk;
-
-dist_mem_gen_w1_1_rom w1_1 (
-  .a(w1_raddr),        // input wire [4 : 0] a
-  .clk(clk),    // input wire clk
-  .qspo(w1_1_rdata)  // output wire [7 : 0] qspo
-);
-dist_mem_gen_w1_2_rom w1_2 (
-  .a(w1_raddr),        // input wire [4 : 0] a
-  .clk(clk),    // input wire clk
-  .qspo(w1_2_rdata)  // output wire [7 : 0] qspo
-);
-dist_mem_gen_w1_3_rom w1_3 (
-  .a(w1_raddr),        // input wire [4 : 0] a
-  .clk(clk),    // input wire clk
-  .qspo(w1_3_rdata)  // output wire [7 : 0] qspo
-);
-dist_mem_gen_w1_4_rom w1_4 (
-  .a(w1_raddr),        // input wire [4 : 0] a
-  .clk(clk),    // input wire clk
-  .qspo(w1_4_rdata)  // output wire [7 : 0] qspo
-);
-dist_mem_gen_w1_5_rom w1_5 (
-  .a(w1_raddr),        // input wire [4 : 0] a
-  .clk(clk),    // input wire clk
-  .qspo(w1_5_rdata)  // output wire [7 : 0] qspo
-);
-dist_mem_gen_w1_6_rom w1_6 (
-  .a(w1_raddr),        // input wire [4 : 0] a
-  .clk(clk),    // input wire clk
-  .qspo(w1_6_rdata)  // output wire [7 : 0] qspo
-);
+  wire [47:0] w1_rdata;
+  dist_mem_gen_w1_rom w1_rom (
+    .a(w1_raddr),        // input wire [4 : 0] a
+    .clk(clk),    // input wire clk
+    .qspo(w1_rdata)  // output wire [47 : 0] qspo
+  );
+  assign w1_1_rdata = w1_rdata[7:0];
+  assign w1_2_rdata = w1_rdata[15:8];
+  assign w1_3_rdata = w1_rdata[23:16];
+  assign w1_4_rdata = w1_rdata[31:24];
+  assign w1_5_rdata = w1_rdata[39:32];
+  assign w1_6_rdata = w1_rdata[47:40];
 
 endmodule 
