582239e85350278b97f401e91d569870ada0c5c1
YzTong <yztong1994@qq.com>
2018-01-26 15:42:05 +0800

    change f4_ram

--- script/perl/generate.pl
+++ script/perl/generate.pl
@@ -2,8 +2,8 @@
 open my $gen_fh,'>','gen.txt'
 	or die "could not open gen.txt:$!";
 
-	for (my $j = 1; $j < 7; $j++) {
+	for (my $j = 1; $j < 17; $j++) {
 			my $i = ($j-1)*16;
 			my $b = $i + 15;
-			print $gen_fh "	assign f2_$j\_rdata = f2_rdata[$b:$i];\n";
+			print $gen_fh "	assign f4_$j\_rdata = f4_rdata[$b:$i];\n";
 }
--- src/memory/f4_ram.v
+++ src/memory/f4_ram.v
@@ -1,7 +1,7 @@
 //==================================================================================================
 //  Filename      : f4_ram.v
 //  Created On    : 2018-01-01 15:35:30
-//  Last Modified : 2018-01-07 20:17:47
+//  Last Modified : 2018-01-26 15:40:49
 //  Revision      : 
 //  Author        : YzTong
 //  Company       : UESTC
@@ -90,174 +90,35 @@ module f4_ram(/*autoport*/
 	output wire[15:0] f4_14_rdata;
 	output wire[15:0] f4_15_rdata;
 	output wire[15:0] f4_16_rdata;
-/*
-	reg[15:0]   mem1[195:0];
-	reg[15:0]   mem2[195:0];
-	reg[15:0]   mem3[195:0];
-	reg[15:0]   mem4[195:0];
-	reg[15:0]   mem5[195:0];
-	reg[15:0]   mem6[195:0];
-	
-	/*always @(posedge clk ) begin
-		if(f4_wr_en) begin
-		mem1[f4_waddr] = f3_1_wdata;
-		mem2[f4_waddr] = f3_2_wdata;
-		mem3[f4_waddr] = f3_3_wdata;
-		mem4[f4_waddr] = f3_4_wdata;
-		mem5[f4_waddr] = f3_5_wdata;
-		mem6[f4_waddr] = f3_6_wdata;
-		end
-	end
 
-	assign f3_1_rdata = mem1[f3_raddr];
-	assign f3_2_rdata = mem2[f3_raddr];
-	assign f3_3_rdata = mem3[f3_raddr];
-	assign f3_4_rdata = mem4[f3_raddr];
-	assign f3_5_rdata = mem5[f3_raddr];
-	assign f3_6_rdata = mem6[f3_raddr];
-	*/
-blk_mem_gen_f4_1_ram f4_1 (
+	wire[255:0] f4_wdata,f4_rdata;
+	blk_mem_gen_f4_ram f4_ram (
 	  .clka(clk),    // input wire clka
 	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_1_wdata),    // input wire [15 : 0] dina
+	  .addra(f4_waddr),  // input wire [6 : 0] addra
+	  .dina(f4_wdata),    // input wire [255 : 0] dina
 	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_1_rdata)  // output wire [15 : 0] doutb
+	  .addrb(f4_raddr),  // input wire [6 : 0] addrb
+	  .doutb(f4_rdata)  // output wire [255 : 0] doutb
 	);
-blk_mem_gen_f4_2_ram f4_2 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_2_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_2_rdata)  // output wire [15 : 0] doutb
-	);
-blk_mem_gen_f4_3_ram f4_3 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_3_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_3_rdata)  // output wire [15 : 0] doutb
-	);
-blk_mem_gen_f4_4_ram f4_4 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_4_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_4_rdata)  // output wire [15 : 0] doutb
-	); 
-blk_mem_gen_f4_5_ram f4_5 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_5_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_5_rdata)  // output wire [15 : 0] doutb
-	);
-blk_mem_gen_f4_6_ram f4_6 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_6_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_6_rdata)  // output wire [15 : 0] doutb
-	);
-blk_mem_gen_f4_7_ram f4_7 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_7_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_7_rdata)  // output wire [15 : 0] doutb
-	);
-blk_mem_gen_f4_8_ram f4_8 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_8_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_8_rdata)  // output wire [15 : 0] doutb
-	);  
-blk_mem_gen_f4_9_ram f4_9 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_9_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_9_rdata)  // output wire [15 : 0] doutb
-	);
-blk_mem_gen_f4_10_ram f4_10 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_10_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_10_rdata)  // output wire [15 : 0] doutb
-	);
-blk_mem_gen_f4_11_ram f4_11 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_11_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_11_rdata)  // output wire [15 : 0] doutb
-	);
-blk_mem_gen_f4_12_ram f4_12 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_12_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_12_rdata)  // output wire [15 : 0] doutb
-	);  
-blk_mem_gen_f4_13_ram f4_13 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_13_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_13_rdata)  // output wire [15 : 0] doutb
-	);
-blk_mem_gen_f4_14_ram f4_14 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_14_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_14_rdata)  // output wire [15 : 0] doutb
-	);
-blk_mem_gen_f4_15_ram f4_15 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_15_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_15_rdata)  // output wire [15 : 0] doutb
-	);
-blk_mem_gen_f4_16_ram f4_16 (
-	  .clka(clk),    // input wire clka
-	  .wea(f4_wr_en),      // input wire [0 : 0] wea
-	  .addra(f4_waddr),  // input wire [7 : 0] addra
-	  .dina(f4_16_wdata),    // input wire [15 : 0] dina
-	  .clkb(clk),    // input wire clkb
-	  .addrb(f4_raddr),  // input wire [7 : 0] addrb
-	  .doutb(f4_16_rdata)  // output wire [15 : 0] doutb
-	);    
+	assign f4_wdata = {f4_16_wdata,f4_15_wdata,f4_14_wdata,f4_13_wdata,
+		f4_12_wdata,f4_11_wdata,f4_10_wdata,f4_9_wdata,f4_8_wdata,f4_7_wdata,
+		f4_6_wdata,f4_5_wdata,f4_4_wdata,f4_3_wdata,f4_2_wdata,f4_1_wdata};
+	assign f4_1_rdata = f4_rdata[15:0];
+	assign f4_2_rdata = f4_rdata[31:16];
+	assign f4_3_rdata = f4_rdata[47:32];
+	assign f4_4_rdata = f4_rdata[63:48];
+	assign f4_5_rdata = f4_rdata[79:64];
+	assign f4_6_rdata = f4_rdata[95:80];
+	assign f4_7_rdata = f4_rdata[111:96];
+	assign f4_8_rdata = f4_rdata[127:112];
+	assign f4_9_rdata = f4_rdata[143:128];
+	assign f4_10_rdata = f4_rdata[159:144];
+	assign f4_11_rdata = f4_rdata[175:160];
+	assign f4_12_rdata = f4_rdata[191:176];
+	assign f4_13_rdata = f4_rdata[207:192];
+	assign f4_14_rdata = f4_rdata[223:208];
+	assign f4_15_rdata = f4_rdata[239:224];
+	assign f4_16_rdata = f4_rdata[255:240];
+
 endmodule 
